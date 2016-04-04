a443f6a535b7ee24ca2977f3477a39bc3d098213
Chase McElroy <ichasem@gmail.com>
2016-04-04 14:51:12 -0700

    Designed and created login screen with material design classes for buttons and and views

---
+++ devslopes-showcase-c/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  devslopes-showcase-c
+//
+//  Created by Chase McElroy on 4/1/16.
+//  Copyright © 2016 Chase McElroy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
