306039fb9b1b704ca3ba7e7a42874ed67b2d0000
Chase McElroy <ichasem@gmail.com>
2016-04-06 17:46:39 -0700

    Added a Feed View Controller and designed and created the table view and table view cell

---
+++ devslopes-showcase-c/FeedVC.swift
@@ -0,0 +1,38 @@
+//
+//  FeedVC.swift
+//  devslopes-showcase-c
+//
+//  Created by Chase McElroy on 4/6/16.
+//  Copyright © 2016 Chase McElroy. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+    }
+
+
+
+}
---
+++ devslopes-showcase-c/PostCell.swift
@@ -0,0 +1,35 @@
+//
+//  PostView.swift
+//  devslopes-showcase-c
+//
+//  Created by Chase McElroy on 4/6/16.
+//  Copyright © 2016 Chase McElroy. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var showcaseImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+    
+    override func drawRect(rect: CGRect) {
+        profileImg.layer.cornerRadius = profileImg.frame.size.width / 2
+        profileImg.clipsToBounds = true
+        
+        showcaseImg.clipsToBounds = true
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
